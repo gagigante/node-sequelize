409f1144fc5671ea459bd913cacc86d522ee99f8
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-03-08 14:59:05 -0300

    Initial commit

---
+++ src/controllers/ReportController.js
@@ -0,0 +1,34 @@
+const { Op } = require('sequelize'); 
+const User = require('../models/User');
+
+module.exports = {
+  async show(req, res) {
+    const users = await User.findAll({
+      attributes: ['name', 'email'],
+      where: {
+        email: {
+          [Op.like]: '%outlook.com'
+        }
+      },
+      include: [
+        { 
+          association: 'addresses', 
+          where: { 
+            street: 'Rua Calixtro Finelli'
+          }
+        },
+        { 
+          association: 'techs',
+          required: false,
+          where: {
+            name: {
+              [Op.like]: 'React%'
+            }
+          }
+        },
+      ]
+    })
+
+    return res.json(users);
+  }
+};
